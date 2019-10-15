f940b33919ec45ebf4797166435773d7f9f15b3b
Richard L Zarth III <rlziii@icloud.com>
2019-10-14 20:41:13 -0400

    Initial Commit

---
+++ ConvertMe WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ConvertMe WatchKit Extension
+//
+//  Created by Richard Zarth on 10/14/19.
+//  Copyright © 2019 Richard L Zarth III. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ConvertMe WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  ConvertMe WatchKit Extension
+//
+//  Created by Richard Zarth on 10/14/19.
+//  Copyright © 2019 Richard L Zarth III. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
